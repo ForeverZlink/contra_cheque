ac57a110d0b0d085e8c7ec377f830dddb2679802
St;ZlinkG <carloscunha080@gmail.com>
2022-03-12 21:36:26 -0300

    Criei um método para a classe Tribute que calcula o valor do tributo. Adicionei na classe Employer um campo de faz referençia a classe office. Na função Main, criei algumas instancias de objeto office e tribute.

--- Program.cs
+++ Program.cs
@@ -8,21 +8,35 @@ namespace contra_cheque
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Digite o nome da empresa:\t");
+            
             string enterprise_name = Console.ReadLine();
             int key_of_enterprise  =33;
             string cnpj ="1333,jjj-9";
             string andress = "Estrada Teresópolis ";
+            
             Enterprise enterprise_1= new Enterprise(
                     enterprise_name,key_of_enterprise,
                     cnpj, andress);
+
+            int    key_of_office    =  1;
+            string name_of_office   = "progammer";
+            float  salary_of_office =  3000;
+            Office progammer = new Office(key_of_office,name_of_office,salary_of_office);
+
             
             string birth        = "10/10/1999";
-            string name         = "Carlos Henrique Cunha de Oliveira";
+            string name_of_employer         = "Carlos Henrique Cunha de Oliveira";
             int    registration = 202211;
+
             Employer empregado = new Employer(enterprise_1, 
-                registration, name, birth);
-            Console.WriteLine(empregado.Enterprise_where_work.Name,enterprise_name);
+                registration, name_of_employer, birth,progammer);
+            
+            string name_of_tribute = "FGTS";
+            int    key_of_tribute  =  1;
+            float  aliquot         = 2.0F;
+            Tribute  FGTS   = new Tribute (name_of_tribute, aliquot,key_of_tribute);
+            float value_of_fgts = FGTS.CalculeTributeValue(empregado.Office.Remuneration);
+            Console.WriteLine($"Com o salário de {empregado.Office.Remuneration}, o fgts será de {value_of_fgts}");
         }
         
     }
@@ -65,20 +79,31 @@ namespace contra_cheque
             this.Key        = Key;
 
         }
+        public float CalculeTributeValue (float salary){
+            float value = (salary*Aliquot)/100;
+            return value;
+
+        }
 
     }
     class Employer{
         public Enterprise Enterprise_where_work;
+        public Office     Office;
         public int    Registration;
         public string Name;
         public string Date_of_birth;
+        
 
-        public Employer(Enterprise Enterprise_where_work, int registration, string name_of_employer, string date_of_birth){
+        public Employer(Enterprise Enterprise_where_work,
+                        int registration, string name_of_employer, 
+                        string date_of_birth, Office name_of_office){
             this.Enterprise_where_work    = Enterprise_where_work;
             this.Registration             = registration;
             this.Name                     = Name;
             this.Date_of_birth            = Date_of_birth;
+            this.Office                   = name_of_office;
         }
+
     }
 }
 
