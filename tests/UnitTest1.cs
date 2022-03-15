adf39ed29899cf58ba6313e4df82f103effce0e1
St;ZlinkG <carloscunha080@gmail.com>
2022-03-14 21:23:46 -0300

    Criei um novo de diretório de testes que usa o mstest

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+
+    {
+        int hello =2;
+        Assert.AreEqual(hello, 2);
+    }
+}
